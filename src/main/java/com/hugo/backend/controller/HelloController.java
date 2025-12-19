acb7547849609bf09f2ac0dbcc1569ee145c94c8
hugopracticas <hugoloyolamaya@gmail.com>
2025-12-19 15:44:17 -0600

    Cambios en textos

--- src/main/java/com/hugo/backend/controller/HelloController.java
+++ src/main/java/com/hugo/backend/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/api/hello")
     public String hello() {
-        return "Hola desde Spring Boot en AWS 😎";
+        return "Hola desde Spring Boot AWS 😎";
     }
 }
